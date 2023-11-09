279181cf60109d68bdf3c5d4b051382e94ec7612
Abduljebar Sani <Abduljebar49@gmail.com>
2023-11-09 18:45:45 +0300

    UI done

--- lib/funtions/constant.dart
+++ lib/funtions/constant.dart
@@ -1,3 +1,4 @@
+import 'package:dev_test/models/comments_model.dart';
 import 'package:flutter/material.dart';
 
 Color selectedColor = const Color(0xFFFFDD5E);
@@ -8,3 +9,83 @@ Color getActiveColor(int index, int selectedIndex) {
   }
   return unselectedColor;
 }
+
+var comment = Comment(
+  avatarUrl: 'assets/avatar1.jpg',
+  name: 'John Doe',
+  username: '@Kerryjo',
+  comment: 'This is a great comment!',
+  replies: 0,
+  hours: 8,
+  commentCount: 15,
+);
+
+final List<Comment> comments = [
+  Comment(
+    avatarUrl: 'assets/images/image1.png',
+    name: 'Kerry Johns',
+    username: '@Kerryjo',
+    comment:
+        'Lorem ipsum dolor sit amet consectetur. Aliquam sagittis ullamcorper 😃 amet justo quisque 😃😃 ullamcorper volutpat. Donec feugiat diam et tellus in habitant viverra duis. ',
+    replies: 15,
+    hours: 3,
+    commentCount: 15,
+  ),
+  Comment(
+    avatarUrl: 'assets/images/image2.png',
+    name: 'Kerry Johns',
+    username: '@Kerryjo',
+    comment:
+        'Lorem ipsum dolor sit amet consectetur. Aliquam sagittis ullamcorper 😃 amet justo quisque 😃😃 ullamcorper volutpat. Donec feugiat diam et tellus in habitant viverra duis. ',
+    replies: 0,
+    hours: 8,
+    commentCount: 0,
+  ),
+  Comment(
+    avatarUrl: 'assets/images/image3.png',
+    name: 'Kerry Johns',
+    username: '@Kerryjo',
+    comment:
+        'Lorem ipsum dolor sit amet consectetur. Aliquam sagittis ullamcorper 😃 amet justo quisque 😃😃 ullamcorper volutpat. Donec feugiat diam et tellus in habitant viverra duis. ',
+    replies: 15,
+    hours: 3,
+    commentCount: 15,
+  ),
+  Comment(
+    avatarUrl: 'assets/images/image4.png',
+    name: 'Kerry Johns',
+    username: '@Kerryjo',
+    comment:
+        'Lorem ipsum dolor sit amet consectetur. Aliquam sagittis ullamcorper 😃 amet justo quisque 😃😃 ullamcorper volutpat. Donec feugiat diam et tellus in habitant viverra duis. ',
+    replies: 0,
+    hours: 8,
+    commentCount: 15,
+  ),
+];
+
+final List<String> emojis = [
+  "😃",
+  "🚀",
+  "🐱",
+  "🍔",
+  "🌸",
+  "🎉",
+  "❤️",
+  "🔥",
+  "🎈",
+  "🌞",
+  "🌈",
+  "🎁",
+  "😃",
+  "🚀",
+  "🐱",
+  "🍔",
+  "🌸",
+  "🎉",
+  "❤️",
+  "🔥",
+  "🎈",
+  "🌞",
+  "🌈",
+  "🎁",
+];
